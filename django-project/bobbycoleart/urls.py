9f01b2b28fcdf1b83514a5fae4d5e8a099b5d9a2
aragon <e.g.aragon2015@gmail.com>
2022-05-26 10:30:53 -0500

    tutorial site complete

--- django-project/bobbycoleart/urls.py
+++ django-project/bobbycoleart/urls.py
@@ -14,19 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.urls import include
-from django.views.generic import RedirectView
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
 ]
-urlpatterns += [
-    path('blog/', include('blog.urls')),
-]
-urlpatterns += [
-    path('', RedirectView.as_view(url='blog/', permanent=True)),
-]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
