2d9124bb895920a17c676e5c8a8b25cf6c1008c8
aragon <e.g.aragon2015@gmail.com>
2022-05-25 23:20:09 -0500

    set up the skeleton of the site

--- django-project/bobbycoleart/settings.py
+++ django-project/bobbycoleart/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #OUR APPS
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
